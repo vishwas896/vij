1637945e9c3d7b7720a9853895dd2fff4bf5d2f7
VIJ <vij.life0@gmail.com>
2025-08-12 09:57:36 +0000

    auth/configuration-not-found error show when sign up

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC9_UHK-HyNsExr7CPN0Nu8lbHwgH4Fx0g",
   authDomain: "vij0-2626c.firebaseapp.com",
   projectId: "vij0-2626c",
-  storageBucket: "vij0-2626c.appspot.com",
+  storageBucket: "vij0-2626c.firebasestorage.app",
   messagingSenderId: "302997831175",
   appId: "1:302997831175:web:a315f63d18d2c681f9b4a2"
 };
